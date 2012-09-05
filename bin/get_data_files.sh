fbefe8206435251d9b8f9e5627ddae117e12db4a
Grant Bremer <gbremer@gmail.com>
2012-09-04 22:45:59 -0400

    Initial version

---
+++ bin/get_data_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -c http://www.greenteapress.com/thinkstats/2002FemPreg.dat.gz
+wget -c http://www.greenteapress.com/thinkstats/2002FemResp.dat.gz
+wget -c http://www.greenteapress.com/thinkstats/2002Male.dat.gz
